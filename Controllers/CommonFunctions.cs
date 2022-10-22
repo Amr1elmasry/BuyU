598ef4e6e704360286f359532b76653fb38c914c
Amr1elmasry <56364993+Amr1elmasry@users.noreply.github.com>
2022-10-22 14:35:06 +0200

    ViewOrders&EmptyCart&SomeJS !

---
+++ Controllers/CommonFunctions.cs
@@ -0,0 +1,18 @@
+﻿using BuyU.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BuyU.Controllers
+{
+    public static class CommonFunctions
+    {
+        public static async Task<ApplicationUser> UserIdAsync(UserManager<ApplicationUser> _userManager , ClaimsPrincipal User)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (user!=null)
+                return (user);
+            return (null);
+        }
+    }
+}
--- Controllers/UserCartController.cs
+++ Controllers/UserCartController.cs
@@ -26,23 +26,22 @@ namespace BuyU.Controllers
 
         public async Task<IActionResult> CartProducts()
         {
-            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var user = await CommonFunctions.UserIdAsync(_userManager, User);
             ViewData["userName"] = user.UserName;
-            var userId = user.Id;
-            var cart = await _context.Carts.Include(p=>p.Products).Include(p=>p.CartProduct).SingleOrDefaultAsync(c => c.UserId == userId);
+            var cart = await _context.Carts.Include(p=>p.Products).Include(p=>p.CartProduct).SingleOrDefaultAsync(c => c.UserId == user.Id);
             if (cart == null)
                 return View(new CartsViewModel());
             int totalprice = (int)cart.CartProduct.Sum(p => p.Product.Price * p.Quantity);
             ViewData["TotalPrice"] = totalprice;
             var cartsViewModel = new CartsViewModel();
-            cartsViewModel.UserId = userId;
+            cartsViewModel.UserId = user.Id;
             cartsViewModel.cartProducts = cart.CartProduct;
             return View(cartsViewModel);
         }
         [HttpPost]
         public async Task<IActionResult> CartProductsAsync(CartsViewModel model)
         {
-            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var user = await CommonFunctions.UserIdAsync(_userManager, User);
             ViewData["userName"] = user.UserName;
             var cart = await _context.Carts.Include(p => p.Products).SingleOrDefaultAsync(c => c.UserId == user.Id);
             foreach (var item in cart.CartProduct)
@@ -58,26 +57,25 @@ namespace BuyU.Controllers
 
         public async Task<IActionResult> AddToCart(int id)
         {
-            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var user = await CommonFunctions.UserIdAsync(_userManager, User);
             var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
             user = _context.Users.Include(c => c.Cart).SingleOrDefault(u=>u.Id==user.Id);
-            if (user.Cart == null)
+            if (user.CartId == null)
             {
                 var Newcart = new Cart { UserId = user.Id, User = user };
-                await _context.Carts.AddAsync(new Cart { UserId = user.Id, User = user });
-                user.CartId = Newcart.CartId;
+                await _context.Carts.AddAsync(new Cart { UserId = user.Id, User = user});
                 await _context.SaveChangesAsync();
             }
+            user.CartId = user.Cart.CartId;
+            _context.Users.Update(user);
             _context.CartProduct.Add(new CartProduct { CartId = (int)user.CartId, ProductId = id, Quantity = 1, dateTime = DateTime.Now, Cart = user.Cart });
             await _context.SaveChangesAsync();
             ViewData["userName"] = user.UserName;
-            _toastNotification.AddSuccessToastMessage("Product added successfully");
+            //_toastNotification.AddSuccessToastMessage("Product added successfully");
 
-            return RedirectToAction("Index", "Home");
+            return Ok();
 
         }
-
-
         public async Task<IActionResult> RemoveProduct(int id)
         {
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
@@ -85,6 +83,18 @@ namespace BuyU.Controllers
             _context.CartProduct.Remove(cartProduct);
             await _context.SaveChangesAsync();
             _toastNotification.AddSuccessToastMessage("Product removed successfully");
+            return RedirectToAction("CartProducts");
+        }
+
+
+        public async Task<IActionResult> EmptyCart()
+        {
+            var user = await CommonFunctions.UserIdAsync(_userManager, User);
+            var cart = await _context.Carts.Include(c=>c.CartProduct).SingleOrDefaultAsync(c =>c.UserId==user.Id);
+            cart.CartProduct.Clear();
+            _context.Carts.Update(cart);
+            await _context.SaveChangesAsync();
+            //_toastNotification.AddSuccessToastMessage("All Products are removed successfully");
             return RedirectToAction("CartProducts");
         }
     }
